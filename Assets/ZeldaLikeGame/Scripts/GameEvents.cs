a4a17fc0ca3cb54f2f27c9e7a2ad5d22461cc16d
IgorPaliyD <54597623+IgorPaliyD@users.noreply.github.com>
2021-04-30 14:53:53 +0300

    30.04.2021
    Some Events system integration start

---
+++ Assets/ZeldaLikeGame/Scripts/GameEvents.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class GameEvents : MonoBehaviour
+{
+    public static GameEvents current;
+
+    private void Awake(){
+        current = this;
+    }
+    public event Action onItemTriggerEnter;
+    public void ItemTriggerEnter(){
+        if(onItemTriggerEnter!=null){
+            onItemTriggerEnter();
+        }
+    } 
+}
---
+++ Assets/ZeldaLikeGame/Scripts/PickUpIteController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpIteController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.onItemTriggerEnter += OnItemPicked;
+    }
+    public void OnItemPicked(){
+        
+    }
+
+   
+}
